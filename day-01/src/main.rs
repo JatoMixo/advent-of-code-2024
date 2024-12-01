f134e3cc8638da8e75d47e6bb89f9e427173a1f2
JatoMixo <jatomixo@outlook.es>
2024-12-01 17:31:00 +0100

    completed day 1 part 1

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -15,4 +15,27 @@ fn main() {
     println!("============= DAY 01 =============");
     
     let data = get_input_data();
+
+    let rows = data.split("\r\n").collect::<Vec<&str>>();
+
+    let mut left_values = Vec::new();
+    let mut right_values = Vec::new();
+
+    rows.iter().for_each(|&row| {
+        let (left, right) = row.split_once("   ").unwrap();
+
+        left_values.push(left.parse::<i32>().unwrap());
+        right_values.push(right.parse::<i32>().unwrap());
+    });
+
+    // TODO: Implement own sort function
+    left_values.sort();
+    right_values.sort();
+
+    let total_distance: i32 = left_values.iter().enumerate().map(|(index, &left_value)| {
+        (left_value - right_values[index]).abs()
+    })
+    .sum();
+
+    println!("{}", total_distance);
 }
