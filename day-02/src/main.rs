80c67dfd404e17031869c6c72b393fe50f249520
JatoMixo <jatomixo@proton.me>
2024-12-03 19:34:00 +0100

    completed day 2 part 1

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -13,6 +13,42 @@ fn get_input_data() -> String {
 
 fn main() {
     println!("============= DAY 02 =============");
-    
+
     let data = get_input_data();
+    let mut reports = data.split("\n").collect::<Vec<&str>>();
+    reports.pop(); // Ignore last line jump.
+
+    let safe_reports_count: i32 = reports.iter().map(|report| {
+
+        let levels = report
+            .split(" ")
+            .map(|level_str| {
+                level_str.parse::<i32>().unwrap()
+            }).collect::<Vec<i32>>();
+
+        if levels[0] - levels[1] == 0 {
+            return 0;
+        }
+
+        let direction = (levels[0] - levels[1]) / (levels[0] - levels[1]).abs();
+
+        for level_index in 0..levels.len() - 1 {
+            let distance = (levels[level_index] - levels[level_index + 1]).abs();
+
+            if distance < 1 || distance > 3 {
+                return 0;
+            }
+
+            let current_direction = (levels[level_index] - levels[level_index + 1]) / distance;
+
+            if current_direction != direction {
+                return 0;
+            }
+        }
+
+        1
+    })
+    .sum();
+
+    println!("{}", safe_reports_count);
 }
