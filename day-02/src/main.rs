7df136578feb0cb9d90cde29060793a16e94bb8f
JatoMixo <jatomixo@outlook.es>
2024-12-04 21:35:21 +0100

    just need to solve a small thing now

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -15,8 +15,7 @@ fn main() {
     println!("============= DAY 02 =============");
 
     let data = get_input_data();
-    let mut reports = data.split("\n").collect::<Vec<&str>>();
-    reports.pop(); // Ignore last line jump.
+    let reports = data.split("\r\n").collect::<Vec<&str>>();
 
     let safe_reports_count: i32 = reports.iter().map(|report| {
 
@@ -26,29 +25,76 @@ fn main() {
                 level_str.parse::<i32>().unwrap()
             }).collect::<Vec<i32>>();
 
-        if levels[0] - levels[1] == 0 {
-            return 0;
+
+        let report_safety = check_report(levels);
+        match report_safety {
+            false => 0,
+            true => 1,
         }
-
-        let direction = (levels[0] - levels[1]) / (levels[0] - levels[1]).abs();
-
-        for level_index in 0..levels.len() - 1 {
-            let distance = (levels[level_index] - levels[level_index + 1]).abs();
-
-            if distance < 1 || distance > 3 {
-                return 0;
-            }
-
-            let current_direction = (levels[level_index] - levels[level_index + 1]) / distance;
-
-            if current_direction != direction {
-                return 0;
-            }
-        }
-
-        1
     })
     .sum();
 
     println!("{}", safe_reports_count);
+}
+
+fn check_report(mut report: Vec<i32>) -> bool {
+    if report[0] - report[1] == 0 {
+        return false;
+    }
+
+    let direction = (report[0] - report[1]) / (report[0] - report[1]).abs();
+
+    for level_index in 0..report.len() - 1 {
+        if check_level(&mut report, direction, level_index) {
+            continue;
+        }
+
+        // Check if invalid would be solved with dampener
+
+        let mut first_removed = report.clone();
+        first_removed.remove(level_index);
+
+        let mut second_removed = report.clone();
+        second_removed.remove(level_index + 1);
+
+        if check_report_no_dampener(first_removed) || check_report_no_dampener(second_removed) {
+            return true;
+        }
+
+        return false;
+    }
+
+    true
+}
+
+fn check_report_no_dampener(report: Vec<i32>) -> bool {
+    if report[0] - report[1] == 0 {
+        return false;
+    }
+
+    let direction = (report[0] - report[1]) / (report[0] - report[1]).abs();
+
+    for level_index in 0..report.len() - 1 {
+        if !check_level(&report, direction, level_index) {
+            return false;
+        }
+    }
+
+    true
+}
+
+fn check_level(levels: &Vec<i32>, direction: i32, index: usize) -> bool {
+    let distance = (levels[index] - levels[index + 1]).abs();
+
+    if distance < 1 || distance > 3 {
+        return false;
+    }
+
+    let current_direction = (levels[index] - levels[index + 1]) / distance;
+
+    if current_direction != direction {
+        return false;
+    }
+
+    true
 }
