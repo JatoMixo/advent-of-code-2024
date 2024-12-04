10597cf9c9f3c76f3d0b2f2783fee4bb6f08cc4b
JatoMixo <jatomixo@outlook.es>
2024-12-04 22:23:01 +0100

    completed day 3 part 1

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -15,4 +15,25 @@ fn main() {
     println!("============= DAY 03 =============");
     
     let data = get_input_data();
+    
+    let mut split_data = data.split("mul(").collect::<Vec<&str>>();
+    split_data.drain(0..1);
+
+    let mut result = 0;
+
+    for &data_fragment in split_data.iter() {
+        let multiplied_numbers = data_fragment.split(")").collect::<Vec<&str>>()[0];
+
+        if !multiplied_numbers.contains(",") {
+            continue;
+        }
+
+        let (first, second) = multiplied_numbers.split_once(",").unwrap();
+        let first = first.parse::<u32>().unwrap_or(0);
+        let second = second.parse::<u32>().unwrap_or(0);
+
+        result += first * second;
+    };
+
+    println!("{}", result);
 }
